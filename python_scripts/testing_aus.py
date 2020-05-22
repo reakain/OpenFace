02c03665711b7412134ff91df6352dddea631ae2
Reakain <reakain@users.noreply.github.com>
2020-05-22 15:30:45 -0400

    Updated python to pull all regression and classification topic data

--- python_scripts/testing_aus.py
+++ python_scripts/testing_aus.py
@@ -8,23 +8,28 @@ if __name__ == '__main__':
 
     print("Do we get live prints before ZMQ?", flush=True)
     context = zmq.Context()
-    socket = context.socket(zmq.SUB)
+    regsocket = context.socket(zmq.SUB)
+    classsocket = context.socket(zmq.SUB)
 
     print( "Collecting head pose updates...", flush=True)
 
-    socket.connect ("tcp://localhost:%s" % port)
-    topic_filter = "Regression_AU06:"
+    regsocket.connect ("tcp://localhost:%s" % port)
+    classsocket.connect("tcp://localhost:%s" % port)
+    topic_filter = "Regression"
     #socket.setsockopt_string(zmq.SUBSCRIBE, topic_filter)
-    socket.subscribe(topic_filter)
+    regsocket.subscribe(topic_filter)
+    classsocket.subscribe("Classification")
 
     while True:
-        au6 = socket.recv().decode('utf8')
+        reg_au = regsocket.recv().decode('utf8')
+        class_au = classsocket.recv().decode('utf8')
 
-        topic, val = au6.split(":")
+        topic, val = reg_au.split(":")
         cheek_val = float(val)
-        #print(socket.recv())
+        print(topic +': %.1f' % (cheek_val), flush=True)
 
-        #print(topic + ':' + val)
+        topic, val = class_au.split(":")
+        cheek_val = float(val)
         print(topic +': %.1f' % (cheek_val), flush=True)
 
         time.sleep(0.01)
