14c3c2fec287a7a7daced5c044547ac072a0b84d
Reakain <reakain@users.noreply.github.com>
2020-06-11 14:56:10 -0400

    Initial setup and beginning the build out of a commandline utility for streaming analysis data from webcam via headless. Based on openfaceoffline and setup using .net core

---
+++ gui/OpenFaceCommandLine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenFaceCommandLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
